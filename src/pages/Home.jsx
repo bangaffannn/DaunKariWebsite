bc278897897cebda1ea50453bb99ed0f60239580
bangaffannn <bangaffannn@gmail.com>
2024-03-07 08:23:37 +0700

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function Home() {
   return (
-    <section className="lg:min-h-screen p-8">
+    <section className="min-h-screen p-8">
       <div className="container mx-auto text-white">
         <div>
-          <div className="flex justify-start items-center grid grid-cols-2">
+          <div className="flex justify-start items-center grid grid-cols-2 mb-10">
             <h1 className="text-4xl md:text-6xl leading-tight font-serif m-5 pt-10">
               BRINGS BACK <span className="font-bold text-primary"> YOUR MEMORIES </span>{" "}
               THROUGH ORIENTAL{" "}
               <span className="font-bold text-primary"> INDONESIAN FOOD. </span>
             </h1>
           </div>
-          <button className="m-5 mb-10 px-4 py-2 rounded-md border border-2 border-gray-700 hover:border-primary hover:scale-110 transition-all duration-300 ease-in-out">Reservation Now</button>
+          <a className="m-5 mb-10 px-4 py-3 rounded-md border border-2 border-gray-700 hover:border-primary transition-all duration-300 ease-in-out cursor-pointer" href="https://wa.me/+628567858218">Reservation Now</a>
         </div>
       </div>
     </section>
